023f1cd7d18cfc65c4990b5a4204cc78aecf290c
jwshin <jwshin@jwshin.com>
2024-05-27 17:50:40 -0700

    dist

---
+++ dist/cli.mjs
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+// src/cli.ts
+async function main() {
+  try {
+    await import("zod");
+  } catch (e) {
+    const RED = "\x1B[31m%s\x1B[0m";
+    console.error(RED, "Please makes sure to install zod first.");
+    process.exit(1);
+  } finally {
+    import("./src-AU7ZK3IA.mjs");
+  }
+}
+main();
